5272ec45f1286815d904c378df759f1b9f4207a9
Marc <marcflament@free.fr>
2024-02-23 13:04:19 +0100

    Started the challenge

--- src/main/java/dev/morling/onebrc/CalculateAverage_baseline.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_baseline.java
@@ -62,6 +62,7 @@ public class CalculateAverage_baseline {
         // .collect(toMap(e -> e.getKey(), e -> Math.round(e.getValue() * 10.0) / 10.0)));
         // System.out.println(measurements1);
 
+        long start = System.currentTimeMillis();
         Collector<Measurement, MeasurementAggregator, ResultRow> collector = Collector.of(
                 MeasurementAggregator::new,
                 (a, m) -> {
@@ -87,6 +88,8 @@ public class CalculateAverage_baseline {
                 .map(l -> new Measurement(l.split(";")))
                 .collect(groupingBy(m -> m.station(), collector)));
 
+        long time = System.currentTimeMillis() - start;
         System.out.println(measurements);
+        System.err.println(STR."time=\{time}");
     }
 }
